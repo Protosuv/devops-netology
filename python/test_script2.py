e60edcb0ac9a6eecd60c3bbd1d4670f7f124e36f
Алексей Суворов <protosuv@gmail.com>
2020-08-15 18:12:51 +0300

    Modified file test_script2.py

--- python/test_script2.py
+++ python/test_script2.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 
 import os
-
-bash_command = ["cd ~/PycharmProjects/netology.devops/devops-netology", "git status"]
+import argparse
+parser = argparse.ArgumentParser(description='Show status of files in Git directory')
+parser.add_argument(
+    '--indir',
+    type=str,
+    default='~/PycharmProjects/netology.devops/devops-netology',
+    help='Input dir for files')
+args = parser.parse_args()
+print(f'Каталог git с проектом: {args.indir}')
+bash_command = ["cd {}".format(args.indir), "git status"]
+# print(bash_command[1])
+# print(type(bash_command))
 result_os = os.popen(' && '.join(bash_command)).read()
 # print(result_os)
 is_change = False
