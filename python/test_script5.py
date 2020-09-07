853dab1f5bc2746249da5b0d3cd34a9f6f7720fa
Алексей Суворов <protosuv@gmail.com>
2020-09-07 21:56:49 +0300

    File test_script4.py has been updated and file test_script5.py added.

--- python/test_script5.py
+++ python/test_script5.py
@@ -2,9 +2,7 @@
 
 import os
 import socket
-import csv
 import json
-import yaml
 
 my_dir = os.getcwd()
 dns_list = os.path.join(my_dir, 'dns_list.csv')
@@ -18,31 +16,21 @@ for result in our_hosts_dns_list:
     our_hosts.setdefault(result, ip_host)
     # print(f'DNS : {result}')
     # print(f'IP : {ip_host}')
-with open(dns_list, newline='') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        dns, ip = row[0], row[1]
-        our_hosts_old[dns] = ip
+
+with open('dns_list.json') as json_file:
+    our_hosts_old = json.load(json_file)
+    for dns in our_hosts_old:
         new_ip = our_hosts[dns]
         old_ip = our_hosts_old[dns]
         if new_ip != old_ip:
             print(f'[ERROR] <{dns}> IP mismatch: <{old_ip}> <{new_ip}>')
+json_file.close()
 # print(our_hosts)
 # print("----")
 # print(our_hosts_old)
-# save dict on CSV file
-with open(dns_list, "w") as a_file:
-    writer = csv.writer(a_file)
-    for key, value in our_hosts.items():
-        writer.writerow([key, value])
-a_file.close()
-# save dict on JSON file
+
+# save dict on JSON file (variant 2)
 with open('dns_list.json', 'w') as json_file:
-    for key, value in our_hosts.items():
-        json.dump({key: value}, json_file)
+    json.dump(our_hosts, json_file)
 json_file.close()
-# save dict on YAML file
-with open('dns_list.yml', 'w') as yaml_file:
-    for key, value in our_hosts.items():
-        yaml.dump({key: value}, yaml_file)
-yaml_file.close()
+
