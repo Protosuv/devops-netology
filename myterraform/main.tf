ee12dc640f496b4d12d61ead219a08bd8e1e3bcf
Alexey Suvorov <protosuv@gmail.com>
2020-12-05 00:36:43 +0300

    Project Terraform: add variables.tf, DB_resources.tf, edit other files.

--- myterraform/main.tf
+++ myterraform/main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = var.aws-region
   shared_credentials_file = "$HOME/.aws/credentials"
   profile                 = "default"
 }
@@ -23,15 +23,16 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_instance" "netology-test" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = local.web_instance_type_map[terraform.workspace]
+  count = local.web_instance_count_map[terraform.workspace]
 
   tags = {
     Name = "Netology-test"
   }
   instance_initiated_shutdown_behavior = "stop"
-
+  associate_public_ip_address = "true"
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_type = "gp2"
@@ -39,4 +40,33 @@ resource "aws_instance" "netology-test" {
     volume_size = 8
   }
 
+}
+
+resource "aws_instance" "proxy" {
+  for_each = local.instances
+  ami = each.value
+  instance_type = each.key
+}
+
+resource "aws_instance" "fileserver" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  tags = {
+    "project": "main"
+  }
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy = true
+    ignore_changes = [tags]
+  }
+}
+
+resource "aws_s3_bucket" "netology-test-bucket" {
+  bucket = "netology-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Testing"
+  }
 }
--- myterraform/versions.tf
+++ myterraform/versions.tf
@@ -5,4 +5,11 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "s3" {
+    bucket = "netology-test-bucket"
+    encrypt = true
+    key = "netology/test-infra/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
 }
