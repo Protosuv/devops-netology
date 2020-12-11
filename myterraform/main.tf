d9b5b92fe656d98c6572c79dc9a2453d8f4e79cf
Alexey Suvorov <protosuv@gmail.com>
2020-12-11 15:01:22 +0300

    Project Terraform on team: edit main.tf for use module aws ec2

--- myterraform/main.tf
+++ myterraform/main.tf
@@ -23,50 +23,78 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = local.web_instance_type_map[terraform.workspace]
-  count = local.web_instance_count_map[terraform.workspace]
+//module "ec2-instance_example_volume-attachment" {
+//  source  = "terraform-aws-modules/ec2-instance/aws//examples/volume-attachment"
+//  version = "2.16.0"
+//}
+
+module "ec2-instance" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  version                = "~> 2.0"
+
+  name                   = "web"
+  instance_count         = 1
+
+  ami                    = "data.aws_ami.ubuntu.id"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
 
   tags = {
     Name = "Netology-test"
-  }
-  instance_initiated_shutdown_behavior = "stop"
-  associate_public_ip_address = "true"
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_type = "gp2"
-    delete_on_termination = "false"
-    volume_size = 8
-  }
-
-}
-
-resource "aws_instance" "proxy" {
-  for_each = local.instances
-  ami = each.value
-  instance_type = each.key
-}
-
-resource "aws_instance" "fileserver" {
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  tags = {
-    "project": "main"
-  }
-  lifecycle {
-    create_before_destroy = true
-    prevent_destroy = true
-    ignore_changes = [tags]
+    Terraform   = "true"
+    Environment = "dev"
   }
 }
 
-resource "aws_s3_bucket" "netology-test-bucket" {
-  bucket = "netology-test-bucket"
-  acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Testing"
-  }
-}
+
+//resource "aws_instance" "web" {
+//  ami           = data.aws_ami.ubuntu.id
+//  instance_type = local.web_instance_type_map[terraform.workspace]
+//  count = local.web_instance_count_map[terraform.workspace]
+//
+//  tags = {
+//    Name = "Netology-test"
+//  }
+//  instance_initiated_shutdown_behavior = "stop"
+//  associate_public_ip_address = "true"
+//  ebs_block_device {
+//    device_name = "/dev/sda1"
+//    volume_type = "gp2"
+//    delete_on_termination = "false"
+//    volume_size = 8
+//  }
+//
+//}
+//
+//resource "aws_instance" "proxy" {
+//  for_each = local.instances
+//  ami = each.value
+//  instance_type = each.key
+//}
+//
+//resource "aws_instance" "fileserver" {
+//  ami = data.aws_ami.ubuntu.id
+//  instance_type = "t3.micro"
+//  tags = {
+//    "project": "main"
+//  }
+//  lifecycle {
+//    create_before_destroy = true
+//    prevent_destroy = true
+//    ignore_changes = [tags]
+//  }
+//}
+//
+//resource "aws_s3_bucket" "netology-test-bucket" {
+//  bucket = "netology-test-bucket"
+//  acl    = "private"
+//
+//  tags = {
+//    Name        = "My bucket"
+//    Environment = "Testing"
+//  }
+//}
